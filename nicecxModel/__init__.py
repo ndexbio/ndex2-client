eee5db8f6d7d5f388568e4deeb7ca1897af0af81
Aaron Gary <agary@ucsd.edu>
2018-01-04 10:59:56 -0800

    Add small def

--- nicecxModel/__init__.py
+++ nicecxModel/__init__.py
@@ -1,1 +1,13 @@
 __author__ = 'aarongary'
+
+def get_status():
+    """
+    Gets the status
+    :return: Message
+    :rtype: string
+    """
+    return 'status good'
+
+
+if __name__ == '__main__':
+    print('in main')

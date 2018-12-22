76d5fb011b66017462390deb98e44148905c9078
Chris Churas <churas.camera@gmail.com>
2018-12-21 16:18:31 -0800

    initial revision

---
+++ tests/test_client.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `nbgwas_rest` package."""
+
+
+import decimal
+import unittest
+
+import numpy as np
+
+from ndex2.client import DecimalEncoder
+
+class TestClient(unittest.TestCase):
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+        pass
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+        pass
+
+    def test_decimalencoder(self):
+        dec = DecimalEncoder()
+
+        # test bytes is returned as string
+        res = dec.default(bytes('hello', 'utf-8'))
+        self.assertTrue(isinstance(res, str))
+
+        # test decimal.Decimal is float
+        res = dec.default(decimal.Decimal(5))
+        self.assertTrue(isinstance(res, float))
+
+        # test numpy.int64 is int
+        res = dec.default(np.int64(12))
+        self.assertTrue(isinstance(res, int))
+
+        # test regular old int comes back as int
+        # res = dec.default(type(1))
+        # self.assertTrue(isinstance(res, int))
+
+
+

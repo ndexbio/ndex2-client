ced41790f62f2bf0ed3cdb7da558040c035501fa
Chris Churas <churas.camera@gmail.com>
2019-01-10 16:03:02 -0800

    added ndex2/tests to ignore list

--- conf.py
+++ conf.py
@@ -84,7 +84,7 @@ if release is None or version is None:
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'vpy36/src']
+exclude_patterns = ['_build', 'vpy36/src', 'ndex2/tests']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.

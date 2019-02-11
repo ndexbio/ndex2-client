c8ea5522689a80725ebce0c3477ac9f06a6cd418
Chris Churas <churas.camera@gmail.com>
2019-02-11 14:10:55 -0800

    initial revision

---
+++ ndex2/constants.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+VALID_ATTRIBUTE_DATATYPES = [
+    'boolean',
+    'double',
+    'integer',
+    'long',
+    'string',
+    'list_of_boolean',
+    'list_of_double',
+    'list_of_integer',
+    'list_of_long',
+    'list_of_string'
+]
+
+NODE_ID = '@id'
+NODE_ATTR_NAME = 'n'
+NODE_ATTR_PROPERTYOF = 'po'
+NODE_ATTR_VALUE = 'v'
+NODE_ATTR_DATATYPE = 'd'
+

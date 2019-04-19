d5d4180ddc393738ca67aba3a806b28f08dfad86
Chris Churas <churas.camera@gmail.com>
2019-04-19 16:47:17 -0700

    initial revision

---
+++ tests/test_legacynetworkxversiononepointonefactory.py
@@ -0,0 +1,280 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for `LegacyNetworkXVersionOnePointOneFactory` class."""
+
+import os
+import unittest
+
+import networkx
+import ndex2
+from ndex2.exceptions import NDExError
+from ndex2.nice_cx_network import LegacyNetworkXVersionOnePointOneFactory
+from ndex2.nice_cx_network import NiceCXNetwork
+
+
+class TestLegacyNetworkXVersionOnePointOneFactory(unittest.TestCase):
+
+    TEST_DIR = os.path.dirname(__file__)
+    WNT_SIGNAL_FILE = os.path.join(TEST_DIR, 'data', 'wntsignaling.cx')
+    DARKTHEME_FILE = os.path.join(TEST_DIR, 'data', 'darkthemefinal.cx')
+    DARKTHEMENODE_FILE = os.path.join(TEST_DIR, 'data',
+                                      'darkthemefinalwithnodevis.cx')
+    GLYPICAN_FILE = os.path.join(TEST_DIR, 'data', 'glypican2.cx')
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+        pass
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+        pass
+
+    def test_none_passed_into_get_graph(self):
+        fac = LegacyNetworkXVersionOnePointOneFactory()
+        try:
+            fac.get_graph(None)
+            self.fail('Expected NDExError')
+        except NDExError as ne:
+            self.assertEqual('input network is None', str(ne))
+
+    def test_empty_network_passed_in_with_various_legacy_modes(self):
+        net = NiceCXNetwork()
+        fac = LegacyNetworkXVersionOnePointOneFactory()
+        g = fac.get_graph(net)
+        self.assertTrue(isinstance(g, networkx.MultiGraph))
+        self.assertEqual(0, len(g))
+        self.assertEqual(0, g.number_of_edges())
+
+        fac = LegacyNetworkXVersionOnePointOneFactory(legacymode=True)
+        g = fac.get_graph(net)
+        self.assertTrue(isinstance(g, networkx.Graph))
+        self.assertEqual(0, len(g))
+        self.assertEqual(0, g.number_of_edges())
+
+        fac = LegacyNetworkXVersionOnePointOneFactory(legacymode=False)
+        g = fac.get_graph(net)
+        self.assertTrue(isinstance(g, networkx.MultiGraph))
+        self.assertEqual(0, len(g))
+        self.assertEqual(0, g.number_of_edges())
+
+        fac = LegacyNetworkXVersionOnePointOneFactory(legacymode=None)
+        g = fac.get_graph(net)
+        self.assertTrue(isinstance(g, networkx.MultiGraph))
+        self.assertEqual(0, len(g))
+        self.assertEqual(0, g.number_of_edges())
+
+        try:
+            LegacyNetworkXVersionOnePointOneFactory(legacymode='blah')
+            self.fail('Expected NDExError')
+        except NDExError as ne:
+            self.assertEqual('blah not a valid value for '
+                             'legacymode parameter', str(ne))
+
+    def test_one_node_no_edge_network(self):
+        net = NiceCXNetwork()
+        net.create_node('first')
+        net.set_name('bob')
+        fac = LegacyNetworkXVersionOnePointOneFactory()
+        g = fac.get_graph(net)
+        self.assertEqual('bob', g.graph['name'])
+        self.assertEqual(1, len(g))
+        self.assertEqual(0, g.number_of_edges())
+        self.assertTrue(0 in g)
+        nodelist = g.nodes(data=True)
+        if float(networkx.__version__) >= 2:
+            self.assertEqual('first', nodelist[0]['name'])
+        else:
+            self.assertEqual('first', nodelist[0][1]['name'])
+
+    def test_one_node_no_edge_network_legacytrue(self):
+        net = NiceCXNetwork()
+        net.create_node('first')
+        net.set_name('bob')
+        fac = LegacyNetworkXVersionOnePointOneFactory(legacymode=True)
+        g = fac.get_graph(net)
+        self.assertEqual('bob', g.graph['name'])
+        self.assertEqual(1, len(g))
+        self.assertEqual(0, g.number_of_edges())
+        self.assertTrue(0 in g)
+        nodelist = g.nodes(data=True)
+        if float(networkx.__version__) >= 2:
+            self.assertEqual('first', nodelist[0]['name'])
+        else:
+            self.assertEqual('first', nodelist[0][1]['name'])
+
+    def test_two_node_one_edge_network(self):
+        net = NiceCXNetwork()
+        net.create_node('first')
+        net.create_node('second')
+        net.create_edge(edge_source=0, edge_target=1)
+        net.set_name('bob')
+        fac = LegacyNetworkXVersionOnePointOneFactory()
+        g = fac.get_graph(net)
+        self.assertEqual('bob', g.graph['name'])
+        self.assertEqual(2, len(g))
+        self.assertEqual(1, g.number_of_edges())
+        self.assertTrue(0 in g)
+        nodelist = g.nodes(data=True)
+        if float(networkx.__version__) >= 2:
+            self.assertEqual('first', nodelist[0]['name'])
+            self.assertEqual('second', nodelist[1]['name'])
+            edgelist = list(g.edges(data=True))
+            self.assertEqual(0, edgelist[0][0])
+            self.assertEqual(1, edgelist[0][1])
+            self.assertEqual(None, edgelist[0][2]['interaction'])
+        else:
+            self.assertEqual('first', nodelist[0][1]['name'])
+            self.assertEqual('second', nodelist[1][1]['name'])
+            edgelist = g.edges(data=True)
+            self.assertEqual(0, edgelist[0][0])
+            self.assertEqual(1, edgelist[0][1])
+            self.assertEqual(None, edgelist[0][2]['interaction'])
+
+    def test_glypican_network_legacyfalse(self):
+        net = ndex2.create_nice_cx_from_file(TestLegacyNetworkXVersionOnePointOneFactory.GLYPICAN_FILE)
+        fac = LegacyNetworkXVersionOnePointOneFactory()
+        g = fac.get_graph(net)
+        self.assertEqual('Glypican 2 network', g.graph['name'])
+        self.assertEqual('', g.graph['reference'])
+        self.assertEqual('Mirko von Elstermann', g.graph['author'])
+        self.assertEqual('Jorge Filmus', g.graph['reviewers'])
+        self.assertEqual('glypican_2pathway', g.graph['labels'])
+        self.assertEqual('APR-2018', g.graph['version'])
+        self.assertEqual('human', g.graph['organism'])
+
+        self.assertEqual('<i>Glypican 2 network</i> was derived from '
+                         'the latest BioPAX3 version of the Pathway '
+                         'Interaction Database (PID) curated by NCI/Nature. '
+                         'The BioPAX was first converted to Extended Binary '
+                         'SIF (EBS) by the PAXTools v5 utility. It was then '
+                         'processed to remove redundant edges, to add a '
+                         '\'directed flow\' layout, and to add a graphic '
+                         'style using Cytoscape Visual Properties. This '
+                         'network can be found in searches using its original '
+                         'PID accession id, present in the \'labels\' '
+                         'property.', g.graph['description'])
+
+        self.assertEqual(2, len(g))
+        self.assertEqual(1, g.number_of_edges())
+        self.assertTrue(0 in g)
+        self.assertTrue(1 in g)
+        nodelist = g.nodes(data=True)
+        if float(networkx.__version__) >= 2:
+            self.assertEqual('MDK', nodelist[0]['name'])
+            self.assertEqual('Protein', nodelist[0]['type'])
+            aliaslist = nodelist[0]['alias']
+            self.assertEqual(2, len(aliaslist))
+            self.assertTrue('uniprot knowledgebase:Q2LEK4' in aliaslist)
+            self.assertTrue('uniprot knowledgebase:Q9UCC7' in aliaslist)
+
+            self.assertEqual('GPC2', nodelist[1]['name'])
+            self.assertEqual('Protein', nodelist[1]['type'])
+            aliaslist = nodelist[1]['alias']
+            self.assertEqual(1, len(aliaslist))
+            self.assertTrue('uniprot knowledgebase:Q8N158' in aliaslist)
+
+            edgelist = list(g.edges(data=True))
+            self.assertEqual(0, edgelist[0][0])
+            self.assertEqual(1, edgelist[0][1])
+            self.assertEqual('in-complex-with', edgelist[0][2]['interaction'])
+            self.assertEqual('false', edgelist[0][2]['directed'])
+        else:
+            self.assertEqual('MDK', nodelist[0][1]['name'])
+            self.assertEqual('Protein', nodelist[0][1]['type'])
+            aliaslist = nodelist[0][1]['alias']
+            self.assertEqual(2, len(aliaslist))
+            self.assertTrue('uniprot knowledgebase:Q2LEK4' in aliaslist)
+            self.assertTrue('uniprot knowledgebase:Q9UCC7' in aliaslist)
+
+            self.assertEqual('GPC2', nodelist[1][1]['name'])
+            self.assertEqual('Protein', nodelist[1][1]['type'])
+            aliaslist = nodelist[1][1]['alias']
+            self.assertEqual(1, len(aliaslist))
+            self.assertTrue('uniprot knowledgebase:Q8N158' in aliaslist)
+
+            edgelist = g.edges(data=True)
+            self.assertEqual(0, edgelist[0][0])
+            self.assertEqual(1, edgelist[0][1])
+            self.assertEqual('in-complex-with', edgelist[0][2]['interaction'])
+            self.assertEqual('false', edgelist[0][2]['directed'])
+
+        # check coordinates
+        self.assertTrue((g.pos[0][0] + 398.3) < 1.0)
+        self.assertTrue((g.pos[0][1] - 70.71) < 1.0)
+        self.assertTrue((g.pos[1][0] + 353.49) < 1.0)
+        self.assertTrue((g.pos[1][1] - 70.71) < 1.0)
+
+    def test_glypican_network_legacymode_true(self):
+        net = ndex2.create_nice_cx_from_file(TestLegacyNetworkXVersionOnePointOneFactory.GLYPICAN_FILE)
+        fac = LegacyNetworkXVersionOnePointOneFactory(legacymode=True)
+        g = fac.get_graph(net)
+        self.assertTrue(isinstance(g, networkx.Graph))
+        self.assertEqual('Glypican 2 network', g.graph['name'])
+        self.assertEqual('', g.graph['reference'])
+        self.assertEqual('Mirko von Elstermann', g.graph['author'])
+        self.assertEqual('Jorge Filmus', g.graph['reviewers'])
+        self.assertEqual('glypican_2pathway', g.graph['labels'])
+        self.assertEqual('APR-2018', g.graph['version'])
+        self.assertEqual('human', g.graph['organism'])
+
+        self.assertEqual('<i>Glypican 2 network</i> was derived from '
+                         'the latest BioPAX3 version of the Pathway '
+                         'Interaction Database (PID) curated by NCI/Nature. '
+                         'The BioPAX was first converted to Extended Binary '
+                         'SIF (EBS) by the PAXTools v5 utility. It was then '
+                         'processed to remove redundant edges, to add a '
+                         '\'directed flow\' layout, and to add a graphic '
+                         'style using Cytoscape Visual Properties. This '
+                         'network can be found in searches using its original '
+                         'PID accession id, present in the \'labels\' '
+                         'property.', g.graph['description'])
+
+        self.assertEqual(2, len(g))
+        self.assertEqual(1, g.number_of_edges())
+        self.assertTrue(0 in g)
+        self.assertTrue(1 in g)
+        nodelist = g.nodes(data=True)
+        if float(networkx.__version__) >= 2:
+            self.assertEqual('MDK', nodelist[0]['name'])
+            self.assertEqual('Protein', nodelist[0]['type'])
+            aliaslist = nodelist[0]['alias']
+            self.assertEqual(2, len(aliaslist))
+            self.assertTrue('uniprot knowledgebase:Q2LEK4' in aliaslist)
+            self.assertTrue('uniprot knowledgebase:Q9UCC7' in aliaslist)
+
+            self.assertEqual('GPC2', nodelist[1]['name'])
+            self.assertEqual('Protein', nodelist[1]['type'])
+            aliaslist = nodelist[1]['alias']
+            self.assertEqual(1, len(aliaslist))
+            self.assertTrue('uniprot knowledgebase:Q8N158' in aliaslist)
+
+            edgelist = list(g.edges(data=True))
+            self.assertEqual(0, edgelist[0][0])
+            self.assertEqual(1, edgelist[0][1])
+            self.assertEqual('in-complex-with', edgelist[0][2]['interaction'])
+            self.assertEqual('false', edgelist[0][2]['directed'])
+        else:
+            self.assertEqual('MDK', nodelist[0][1]['name'])
+            self.assertEqual('Protein', nodelist[0][1]['type'])
+            aliaslist = nodelist[0][1]['alias']
+            self.assertEqual(2, len(aliaslist))
+            self.assertTrue('uniprot knowledgebase:Q2LEK4' in aliaslist)
+            self.assertTrue('uniprot knowledgebase:Q9UCC7' in aliaslist)
+
+            self.assertEqual('GPC2', nodelist[1][1]['name'])
+            self.assertEqual('Protein', nodelist[1][1]['type'])
+            aliaslist = nodelist[1][1]['alias']
+            self.assertEqual(1, len(aliaslist))
+            self.assertTrue('uniprot knowledgebase:Q8N158' in aliaslist)
+
+            edgelist = g.edges(data=True)
+            self.assertEqual(0, edgelist[0][0])
+            self.assertEqual(1, edgelist[0][1])
+            self.assertEqual('in-complex-with', edgelist[0][2]['interaction'])
+            self.assertEqual('false', edgelist[0][2]['directed'])
+
+        # check coordinates
+        self.assertTrue((g.pos[0][0] + 398.3) < 1.0)
+        self.assertTrue((g.pos[0][1] - 70.71) < 1.0)
+        self.assertTrue((g.pos[1][0] + 353.49) < 1.0)
+        self.assertTrue((g.pos[1][1] - 70.71) < 1.0)

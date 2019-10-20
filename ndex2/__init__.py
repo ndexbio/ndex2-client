2eb6b69b842953c76d57daeef518a41f3dc8dd9a
Chris Churas <churas.camera@gmail.com>
2019-10-20 12:07:27 -0700

    switched log message to use logger object. Fix for issue #16

--- ndex2/__init__.py
+++ ndex2/__init__.py
@@ -11,7 +11,8 @@ import numpy as np
 from ndex2cx.nice_cx_builder import NiceCXBuilder
 
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 def get_logger(name, level=logging.DEBUG):
@@ -433,7 +434,7 @@ def create_nice_cx_from_pandas(df, source_field=None, target_field=None,
     if source_field and target_field:
         for index, row in df.iterrows():
             if count % 1000 == 0:
-                logging.info(str(count))
+                logger.info(str(count))
             count += 1
             # =============
             # ADD NODES

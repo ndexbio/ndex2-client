ccf2671561a92e51a50fa956265ba3f255ab9c93
Chris Churas <churas.camera@gmail.com>
2021-03-31 15:10:18 -0700

    added no code coverage declaraction so it will be ignored

--- ndex2cx/parallelCX.py
+++ ndex2cx/parallelCX.py
@@ -1,6 +1,6 @@
 
 
-class ParallelCX(object):
+class ParallelCX(object):  # pragma: no cover
     def __init__(self, cx=None, server=None, username=None, password=None, uuid=None, networkx_G=None, pandas_df=None, filename=None, data=None, **attr):
         self.metadata = {}
         self.context = []

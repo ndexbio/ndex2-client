c8f840c6eb5168359ba8aac496cccb9e3f151812
Chris Churas <churas.camera@gmail.com>
2022-05-19 14:39:48 -0700

    added constants for all CX data types. Also added note about how the cartesianLayout coordinate system worked

--- ndex2/constants.py
+++ ndex2/constants.py
@@ -4,17 +4,68 @@
 Contains constants used by the NDEx2 Python Client
 """
 
+BOOLEAN_DATATYPE = 'boolean'
+"""
+Boolean data type for CX
+"""
+
+DOUBLE_DATATYPE = 'double'
+"""
+Double data type for CX
+"""
+
+INTEGER_DATATYPE = 'integer'
+"""
+Integer data type for CX
+"""
+
+LONG_DATATYPE = 'long'
+"""
+Long data type for CX
+"""
+
+STRING_DATATYPE = 'string'
+"""
+String data type for CX
+"""
+
+LIST_OF_BOOLEAN = 'list_of_boolean'
+"""
+List of Boolean data type for CX
+"""
+
+LIST_OF_DOUBLE = 'list_of_double'
+"""
+List of Double data type for CX
+"""
+
+LIST_OF_INTEGER = 'list_of_integer'
+"""
+List of Integer data type for CX
+"""
+
+LIST_OF_LONG = 'list_of_long'
+"""
+List of Long data type for CX
+"""
+
+LIST_OF_STRING = 'list_of_string'
+"""
+List of String data type for CX
+"""
+
+
 VALID_ATTRIBUTE_DATATYPES = [
-    'boolean',
-    'double',
-    'integer',
-    'long',
-    'string',
-    'list_of_boolean',
-    'list_of_double',
-    'list_of_integer',
-    'list_of_long',
-    'list_of_string'
+    BOOLEAN_DATATYPE,
+    DOUBLE_DATATYPE,
+    INTEGER_DATATYPE,
+    LONG_DATATYPE,
+    STRING_DATATYPE,
+    LIST_OF_BOOLEAN,
+    LIST_OF_DOUBLE,
+    LIST_OF_INTEGER,
+    LIST_OF_LONG,
+    LIST_OF_STRING
 ]
 """
 List of valid attribute data types
@@ -23,6 +74,27 @@ List of valid attribute data types
 CARTESIAN_LAYOUT_ASPECT = 'cartesianLayout'
 """
 Name of opaque aspect containing coordinates of nodes
+
+.. code-block:: text
+
+    "cartesianLayout": [ 
+        {
+            "node": 0,
+            "x": 25.0,
+            "y": 50.0
+        }, {
+            "node": 1,
+            "x": -10.0,
+            "y": -200.0
+        }
+    ]
+    
+.. note::
+
+   Although the name implies a cartesian coordinate system,
+   that is actually wrong. The Y access is inverted so lower 
+   values of Y are rendered higher on a graph. 0,0 is considered
+   upper left corner, but negative values are allowed 
 """
 
 NET_ATTR_NAME = 'n'
@@ -89,7 +161,6 @@ NODE_ATTR_DATATYPE = 'd'
 """
 Key for node attribute data type
 """
-
 
 LAYOUT_NODE = 'node'
 """

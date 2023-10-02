be2d2478d03b900731cb108557195ea7dcd4c34c
Joanna Lenkiewicz <jlenkiewicz@ucsd.edu>
2023-10-02 10:33:35 -0700

    Added handling of default values and function documentation

--- ndex2/ndexclient.py
+++ ndex2/ndexclient.py
@@ -2,12 +2,20 @@ import json
 
 
 class CX2Network(object):
+    """
+    Represents the CX2 network format, allowing for reading, processing, and writing of CX2 data.
+    """
+
     def __init__(self):
+        """
+        Initializes an empty CX2Network instance with default data structures.
+        """
         self.attribute_declarations = None
         self.network_attribute = {}
         self.nodes = {}
         self.edges = {}
-        self.aliases = {"nodes": {}, "edges": {}, "networkAttributes": {}}
+        self.aliases = {"nodes": {}, "edges": {}}
+        self.default_values = {"nodes": {}, "edges": {}}
         self.visual_properties = []
         self.node_bypasses = {}
         self.edge_bypasses = {}
@@ -15,6 +23,12 @@ class CX2Network(object):
         self.status = {}
 
     def create_from_raw_cx2(self, cx2_file_path):
+        """
+        Loads and processes a raw CX2 file into structured data within the instance.
+
+        :param cx2_file_path: Path to the CX2 file to be processed.
+        :type cx2_file_path: str
+        """
         with open(cx2_file_path, 'r') as cx2_file:
             raw_data = json.load(cx2_file)
 
@@ -24,16 +38,18 @@ class CX2Network(object):
                 for aspect, declarations in self.attribute_declarations.items():
                     for key, details in declarations.items():
                         alias = details.get("a", None)
-                        if alias:
+                        if alias and aspect in ["nodes", "edges"]:
                             self.aliases[aspect][alias] = key
+                        default_value = details.get("v", None)
+                        if default_value and aspect in ["nodes", "edges"]:
+                            self.default_values[aspect][key] = default_value
 
             elif 'networkAttributes' in section:
                 for attr in section['networkAttributes']:
                     for key, value in attr.items():
-                        actual_key = self.aliases['networkAttributes'].get(key, key)
-                        declared_type = (self.attribute_declarations['networkAttributes'].get(actual_key, {})
+                        declared_type = (self.attribute_declarations['networkAttributes'].get(key, {})
                                          .get('d', 'string'))
-                        self.network_attribute[actual_key] = self._convert_value(declared_type, value)
+                        self.network_attribute[key] = self._convert_value(declared_type, value)
 
             elif 'nodes' in section:
                 for node in section['nodes']:
@@ -72,6 +88,12 @@ class CX2Network(object):
                 self.opaque_aspects.append(section)
 
     def write_as_raw_cx2(self, output_path):
+        """
+        Writes data from CX2Network object to a raw CX2 formatted JSON file.
+
+        :param output_path: Destination file path for the CX2 formatted output.
+        :type output_path: str
+        """
         output_data = [{
             "CXVersion": "2.0",
             "hasFragments": False
@@ -97,7 +119,6 @@ class CX2Network(object):
         nodes_list = self._replace_with_alias(list(self.nodes.values()), 'nodes')
         edges_list = self._replace_with_alias(list(self.edges.values()), 'edges')
 
-        # Writing nodes, but filter out None x, y, and z
         output_nodes = []
         for node in nodes_list:
             clean_node = {k: v for k, v in node.items() if k not in ['x', 'y', 'z']}
@@ -137,14 +158,38 @@ class CX2Network(object):
             json.dump(output_data, output_file, indent=4)
 
     def _process_attributes(self, aspect_name, attributes):
+        """
+        Processes attributes based on declared types and aliases for the given aspect.
+
+        :param aspect_name: Name of the aspect (e.g., 'nodes', 'edges') for which attributes are being processed.
+        :type aspect_name: str
+        :param attributes: Dictionary of attributes to be processed.
+        :type attributes: dict
+        """
         processed_attrs = {}
+
+        # Initially populate with default values
+        for key, default_value in self.default_values[aspect_name].items():
+            actual_key = self.aliases[aspect_name].get(key, key)
+            processed_attrs[actual_key] = default_value
+
         for key, value in attributes.items():
             actual_key = self.aliases[aspect_name].get(key, key)
             declared_type = self.attribute_declarations[aspect_name].get(actual_key, {}).get('d', 'string')
-            processed_attrs[actual_key] = self._convert_value(declared_type, value)
+            if value is not None:
+                processed_attrs[actual_key] = self._convert_value(declared_type, value)
+
         return processed_attrs
 
     def _convert_value(self, dtype, value):
+        """
+        Converts a value to its appropriate data type based on its declared type.
+
+        :param dtype: Declared data type for the value.
+        :type dtype: str
+        :param value: Value to be converted.
+        :type value: any
+        """
         if dtype == "integer":
             return int(value)
         elif dtype == "double" or dtype == "long":
@@ -157,8 +202,15 @@ class CX2Network(object):
         else:
             return value
 
-    # Convert attribute names back to aliases for nodes and edges
     def _replace_with_alias(self, data_list, aspect):
+        """
+        Replaces attribute names in a data list with their corresponding aliases, if available.
+
+        :param data_list: List of data items (e.g., nodes or edges) with attributes.
+        :type data_list: list
+        :param aspect: Name of the aspect (e.g., 'nodes', 'edges') for which aliases are to be applied.
+        :type aspect: str
+        """
         new_data = []
 
         reverse_aliases = {v: k for k, v in self.aliases[aspect].items()}

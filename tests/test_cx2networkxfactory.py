ad1f54d28847d31b9cecf9a6abee25977de2bab2
Chris Churas <churas.camera@gmail.com>
2023-12-22 15:05:58 -0800

    added roundtrip of demo.cx2 to networkx and back to tests

--- tests/test_cx2networkxfactory.py
+++ tests/test_cx2networkxfactory.py
@@ -2,9 +2,43 @@ import os
 import unittest
 import networkx as nx
 from ndex2.cx2 import CX2NetworkXFactory, CX2Network
+from ndex2.cx2 import NetworkXToCX2NetworkFactory
 
 
 class TestCX2NetworkXFactory(unittest.TestCase):
+
+    def get_node_matching_name(self, cx2net=None, name=None):
+        """
+        Gets 1st node matching name
+
+        :param cx2net:
+        :param name:
+        :return:
+        """
+        for node_id, node_obj in cx2net.get_nodes().items():
+            if 'v' not in node_obj:
+                continue
+            if 'name' not in node_obj['v']:
+                continue
+            if node_obj['v']['name'] == name:
+                return node_obj
+        return None
+
+    def get_edge_matching_name(self, cx2net=None, name=None):
+        """
+
+        :param cx2net:
+        :param name:
+        :return:
+        """
+        for edge_id, edge_obj in cx2net.get_edges().items():
+            if 'v' not in edge_obj:
+                continue
+            if 'name' not in edge_obj['v']:
+                continue
+            if edge_obj['v']['name'] == name:
+                return edge_obj
+        return None
 
     def setUp(self):
         current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -40,6 +74,38 @@ class TestCX2NetworkXFactory(unittest.TestCase):
         graph = self.factory.get_graph(self.cx2network)
         self.assertIn('name', graph.graph)
         self.assertEqual(graph.graph['name'], 'DLoc Hierarchy demo')
+
+    def test_roundtrip(self):
+        graph = self.factory.get_graph(self.cx2network)
+        fac = NetworkXToCX2NetworkFactory()
+        rt_cx2net = fac.get_cx2network(graph)
+
+        for node_name in ['721', '737', '747', '751', '753', '764']:
+            rt_node = self.get_node_matching_name(cx2net=rt_cx2net, name=node_name)
+            orig_node = self.get_node_matching_name(cx2net=self.cx2network, name=node_name)
+            import json
+            print('\nComparing original node with node roundtripped through networkx\n')
+            print(json.dumps(orig_node, indent=2))
+            print('------------------------\n')
+            print(json.dumps(rt_node, indent=2))
+            print(rt_node)
+            print('\n\n\n\n\n\n\n\n\n\n\n')
+            self.assertEqual(rt_node['v'], orig_node['v'])
+            self.assertAlmostEqual(rt_node['x'], orig_node['x'], 0.001)
+            self.assertAlmostEqual(rt_node['y'], orig_node['y'], 0.001)
+            self.assertEqual(rt_node['z'], orig_node['z'])
+
+        for edge_name in ['753 (interacts with) 751',
+                          '747 (interacts with) 721',
+                          '751 (interacts with) 721',
+                          '751 (interacts with) 737',
+                          '764 (interacts with) 747',
+                          '764 (interacts with) 753']:
+            orig_edge = self.get_edge_matching_name(cx2net=self.cx2network, name=edge_name)
+            rt_edge = self.get_edge_matching_name(cx2net=rt_cx2net, name=edge_name)
+            self.assertIsNotNone(orig_edge, edge_name)
+            self.assertIsNotNone(rt_edge)
+            self.assertEqual(rt_edge['v'], orig_edge['v'])
 
 
 if __name__ == '__main__':

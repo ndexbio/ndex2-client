8859e55a0c03fc489119cfab71e1c76241850030
Chris Churas <churas.camera@gmail.com>
2024-03-04 08:58:28 -0800

    added sphinx-copybutton to include copy icon for code snippets. Fix for issue #99

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.autosectionlabel',
-    'sphinx.ext.viewcode'
+    'sphinx.ext.viewcode',
+    'sphinx_copybutton'
 ]
 
 # intersphinx mapping
